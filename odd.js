a04687953f146fbec224f6ad59143c037efc8149
archaemedes <wadecroft@gmail.com>
2021-04-16 20:29:37 +0000

    Created a function to check if numbers are odd

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function(address) {
+  return address % 2 === 1;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("15 is odd: " + isOdd(15));
+console.log("74 is odd: " + isOdd(74));
+console.log("99 is odd: " + isOdd(99));
+console.log("176 is odd: " + isOdd(176));
